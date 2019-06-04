1b4c2443454fe3908b85d5be0ca45b24d4b81d43
jose <160128331@aluno.unb.br>
2019-06-04 00:03:14 -0300

    Mudando a plataforma para flutter

---
+++ lib/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:spotjams/exhibition_bottom_sheet.dart';
+import 'package:spotjams/sliding_cards.dart';
+import 'package:spotjams/tabs.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: <Widget>[
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 8),
+                Header(),
+                SizedBox(height: 40),
+                Tabs(),
+                SizedBox(height: 8),
+                SlidingCardsView(),
+              ],
+            ),
+          ),
+          ExhibitionBottomSheet(),
+        ],
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Text(
+        'Shenzhen',
+        style: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
